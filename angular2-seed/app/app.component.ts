21db9eabb9deb8780f070c8b42932e36432464b8
Natan Danovich <Natan Danovich>
2016-10-26 23:46:03 +0300

    Simple Form

--- angular2-seed/app/app.component.ts
+++ angular2-seed/app/app.component.ts
@@ -1,7 +1,9 @@
 import {Component} from 'angular2/core';
+import {ContactFormComponent} from "./contact-form.component";
 
 @Component({
     selector: 'my-app',
-    template: '<h1>My First Angular 2 App</h1>'
+    template: '<contact-form></contact-form>',
+    directives: [ContactFormComponent]
 })
 export class AppComponent { }
