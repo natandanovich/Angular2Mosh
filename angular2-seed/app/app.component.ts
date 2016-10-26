832e6c00af2c78347737e72c3dc99d60e491b98d
Natan Danovich <Natan Danovich>
2016-10-26 10:26:47 +0300

    Finished Zippy Component

--- angular2-seed/app/app.component.ts
+++ angular2-seed/app/app.component.ts
@@ -1,7 +1,20 @@
 import {Component} from 'angular2/core';
+import {ZippyComponent} from "./zippy.component";
 
 @Component({
     selector: 'my-app',
-    template: '<h1>My First Angular 2 App</h1>'
+    template: `
+                <div class="panelconf">
+                <zippy title="Who can see my stuff?">
+                Content of who can see my stuff</zippy>
+                <zippy title="Whc can contact me?">
+                Content of who can contact me</zippy>
+                </div>`,
+    styles: [`
+                .panelconf{
+                width: 220px;
+                }
+            `] ,
+    directives: [ZippyComponent]
 })
 export class AppComponent { }
---
+++ angular2-seed/app/zippy.component.ts
@@ -0,0 +1,45 @@
+import {Component, Input} from "angular2/core";
+
+@Component({
+    selector: 'zippy',
+    template: `
+            <div class="panel panel-default">
+              <div class="panel-heading">
+                  <div align="left">{{title}}</div> 
+                  <div align="right"><i
+                  class="glyphicon"
+                  [ngClass]="{
+                    'glyphicon-chevron-down': !isOpen,
+                    'glyphicon-chevron-up': isOpen
+                    }" 
+                  (click)="onClick()">
+                  </i></div>
+              </div>
+              <div *ngIf="isOpen == true" class="panel-body">
+                <ng-content></ng-content>
+              </div>
+            </div>
+`,
+    styles: [`
+            .glyphicon {
+            cursor: pointer;
+            padding: 5px;
+            }
+            
+             .panel-heading {
+            font-weight: bold;
+            }
+            
+            .panel-heading:hover {
+            background-color: lightgray;
+            }
+            `]
+})
+
+export class ZippyComponent {
+    isOpen = false;
+@Input() title='';
+    onClick() {
+        this.isOpen = !this.isOpen;
+    }
+}
