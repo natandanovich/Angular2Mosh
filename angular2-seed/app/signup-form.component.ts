1ffb86002c3fc5afec12e0a4f6b7d40936878985
Natan Danovich <Natan Danovich>
2016-11-02 16:32:59 +0200

    minor fix in the template

--- angular2-seed/app/signup-form.component.ts
+++ angular2-seed/app/signup-form.component.ts
@@ -1,15 +1,26 @@
 import {Component} from 'angular2/core';
-import {ControlGroup, Control, Validators} from "angular2/common";
+import {ControlGroup, Control, Validators, FormBuilder} from "angular2/common";
+import {UsernameValidators} from "./usernameValidators";
 
 @Component({
     selector: 'signup-form',
     templateUrl: 'app/signup-form.component.html'
 })
 export class SignUpFormComponent {
-    form = new ControlGroup({
-        username: new Control('',Validators.required),
-        password: new Control('',Validators.required)
-    });
+    // form = new ControlGroup({
+    //     username: new Control('',Validators.required),
+    //     password: new Control('',Validators.required)
+    // });
+
+    form: ControlGroup;
+
+    constructor(fb: FormBuilder){
+        this.form = fb.group({
+            username: ['',Validators.compose([Validators.required, UsernameValidators.cannotContainSpace])],
+            password: ['',Validators.required]
+        });
+    }
+
     signup(){
         console.log(this.form.value)
     }
